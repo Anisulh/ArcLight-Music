d5925e3b5b75b5e7a8f6b3e6e1d695951cd4a968
Anisul <anisulhaque9391@gmail.com>
2022-12-01 18:49:35 -0500

    preparing for deployment

--- server/music_party/settings.py
+++ server/music_party/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import environ
 import dj_database_url
-import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -26,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY', 'sample_secret')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
