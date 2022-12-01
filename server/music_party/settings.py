7506bda4331053cbc8f045a2877cc0ab5d2af3ad
Anisul <anisulhaque9391@gmail.com>
2022-12-01 17:25:24 -0500

    ready server for deployment

--- server/music_party/settings.py
+++ server/music_party/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import environ
 import dj_database_url
-
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -175,7 +175,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
