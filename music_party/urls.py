2ef214b6381e61b271aa6c49a322d95e18dcd293
Anisul <anisulhaque9391@gmail.com>
2022-12-03 01:09:02 -0500

    restructured files

--- music_party/urls.py
+++ music_party/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.urls')),
     path('spotify/', include('spotify.urls')),
-    re_path(r"^(?:.*)?$", TemplateView.as_view(template_name="index.html"))
+    re_path(r'^(?:.*)/$', TemplateView.as_view(template_name='index.html')),
 ]
